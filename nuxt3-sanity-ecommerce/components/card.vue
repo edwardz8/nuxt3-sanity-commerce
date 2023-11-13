d6d99bec5b93126fa2f80a822c7b1c9f42fb2512
Edwards, Zach <edwardszm@ornl.gov>
2023-11-13 10:39:00 -0500

    add products from sanity

--- nuxt3-sanity-ecommerce/components/card.vue
+++ nuxt3-sanity-ecommerce/components/card.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="h-80 w-64 border">
-   <!--  <SanityImage
-    :asset-id="image"
-    auto="format"
-    class="h-72 w-64"
-  />   -->  
+   <img :src="image" class="h-72 w-64" /> 
     <span class="pl-3">
       <h1 class="font-bold text-lg text-gray-100">{{ title }}</h1>
-      <!-- <h1>{{ price }}</h1> -->
+      <h1>{{ price }}</h1>
+      <p>{{ desc }}</p>
     </span>
   </div>
 </template>
@@ -15,9 +12,9 @@
 <script setup>
 const props = defineProps({
   title: String,
- // image: String,
-  // price: Number,
-  // desc: String
+  image: String,
+  price: Number,
+  desc: String
 })
 </script>
 
--- nuxt3-sanity-ecommerce/pages/index.vue
+++ nuxt3-sanity-ecommerce/pages/index.vue
@@ -1,17 +1,20 @@
 <script setup>
-  const query = `*[_type == 'product']{title}`
-  const { data } = useSanityQuery(query)
+const query = `*[_type == 'product']`
+const { data } = await useSanityQuery(query)
+console.log(data.value)
+
+data.value.forEach((currentValue) => {
+    console.log(currentValue.store.previewImageUrl)
+})
+
 </script>
 
 <template>
     <UContainer>
         <h1>Nuxt 3 Sanity Shopify</h1>
         <div class="grid grid-cols-4 justify-items-center gap-y-5">
-            <Card v-for="(product, i) in data" :key="i"
-            :title="product.title"
-            :price="product.price"
-            :desc="product.desc"
-            />
+            <Card v-for="(product, i) in data" :key="i" :title="product.store.title" :price="product.store.price"
+                :desc="product.store.desc" :image="product.store.previewImageUrl" />
         </div>
     </UContainer>
 </template>
